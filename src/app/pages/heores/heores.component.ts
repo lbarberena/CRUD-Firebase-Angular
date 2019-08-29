cc39ca4175701232c136b0fd5a32110e78bca4aa
leonel.barberena <octaviobarberena8@gmail.com>
2019-08-29 15:08:04 -0600

    DELETE Method

--- src/app/pages/heores/heores.component.ts
+++ src/app/pages/heores/heores.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from 'src/app/services/heroes.service';
 import { HeroeModel } from 'src/app/models/heroe.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-heores',
@@ -14,6 +15,23 @@ export class HeoresComponent implements OnInit {
 
   ngOnInit() {
     this.heroesService.getHeroes().subscribe( resp => this.heroes = resp );
+  }
+
+  borrarHeroe( heroe: HeroeModel, i: number ) {
+    Swal.fire({
+      title: '¿Desea continuar?',
+      text: `¿Estas seguro que deseas borrar a ${ heroe.nombre }`,
+      type: 'question',
+      showConfirmButton: true,
+      showCancelButton: true
+    }).then( resp => {
+      if ( resp.value ) {
+        this.heroes.splice(i, 1);
+        this.heroesService.borrarHeroe( heroe.id ).subscribe();
+      }
+    });
+
+    
   }
 
 }
